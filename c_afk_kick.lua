a96aab1da4f46beb8437e4eacbf0e2db56aa4ccf
fribblet56 <fribblet56@gmail.com>
2020-05-19 00:02:46 +0200

    Update c_afk_kick.lua

--- c_afk_kick.lua
+++ c_afk_kick.lua
@@ -2,6 +2,15 @@
  -- https://github.com/fribblet56
  -- https://discord.gg/MDEwtKr
 
- AddEvent("OnKeyPress", function(key)
+local key_list = {}
+
+CreateTimer(function()
+    if #key_list == 0 then return end
+    key_list = {}
     CallRemoteEvent("afk:update")
+end, 1000)
+
+
+AddEvent("OnKeyPress", function(key)
+    table.insert(key_list, key)
 end)
