dfdaa2b0d8edd1a1a2abec7160f05ed53762935c
fribblet56 <fribblet56@gmail.com>
2020-05-18 23:52:40 +0200

    Update s_afk_kick.lua

--- s_afk_kick.lua
+++ s_afk_kick.lua
@@ -2,25 +2,27 @@
  -- https://github.com/fribblet56
  -- https://discord.gg/MDEwtKr
 
-local afk = {}
+ local afk = {}
 
-CreateTimer(function()
-    local now = os.time(os.date("!*t"))
-    for k,v in pairs(GetAllPlayers()) do
-        if GetPlayerMovementMode(v) ~= 0 then break end
-        
-        if afk[v].timer < (now - AFK_KICK_TIME*60) then
-            print("[AFK-KICK] "..GetPlayerName(v).."("..v..") has been kicked for AFK.")
-            KickPlayer(v, AFK_KICK_MESSAGE) 
-        end
-    end
-end, 1000)
-
-AddRemoteEvent("afk:update", function(player)
-    afk[player].timer = os.time(os.date("!*t"))
-end)
-
-AddEvent("OnPlayerJoin", function(player)
-    afk[player] = {}
-    afk[player].timer = os.time(os.date("!*t"))
-end)
+ CreateTimer(function()
+     local now = os.time(os.date("!*t"))
+     for k,v in pairs(GetAllPlayers()) do
+         if GetPlayerMovementMode(v) ~= 0 then break end
+         if afk[v] == nil then break end
+         
+         if afk[v].timer < (now - AFK_KICK_TIME*60) then
+             print("[AFK-KICK] "..GetPlayerName(v).."("..v..") has been kicked for AFK.")
+             KickPlayer(v, AFK_KICK_MESSAGE) 
+         end
+     end
+ end, 1000)
+ 
+ AddRemoteEvent("afk:update", function(player)
+     afk[player].timer = os.time(os.date("!*t"))
+ end)
+ 
+ AddEvent("OnPlayerJoin", function(player)
+     afk[player] = {}
+     afk[player].timer = os.time(os.date("!*t"))
+ end)
+ 
